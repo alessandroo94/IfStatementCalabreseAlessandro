0c20ed46a660088d815d8d3924abc6a1c9321b2c
Alessandro Calabrese <calabrese.alessandro1394@gmail.com>
2023-01-20 11:35:04 +0100

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        String myName = "Alessandro";
+
+        if((myName.length() % 2) == 0){
+            System.out.println("The number of letters in my name are even");
+        }else{
+            System.out.println("The number of letters in my name are odd");
+        }
+    }
+}
